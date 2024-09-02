f717b824b76ec9f4c8959889a3b6072c00bbbc04
Ed <silva.email@yahoo.com>
2024-09-01 19:00:56 -0500

    Possession Function

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-/* 
-Basketball Scoreboard
-Pseudocode for the Basketball Scoreboard App:
-
+/*
 Initialize variables:
 
 homeScore = 0
 visitorScore = 0
+possession = "home" // Start with home team having possession
 Get references to HTML elements:
 
-homeScoreDisplay (the element that will show the home team's score)
-visitorScoreDisplay (the element that will show the visitor team's score)
-onePointBtn, twoPointBtn, threePointBtn (the buttons for adding points)
+homeScoreDisplay
+visitorScoreDisplay
+onePointBtn, twoPointBtn, threePointBtn
+possessionIndicator (the element that will show which team has possession)
 Add event listeners to buttons:
 
-When onePointBtn is clicked:
-Add 1 to homeScore or visitorScore depending on which team's button was clicked.
-Update homeScoreDisplay or visitorScoreDisplay with the new score.
-Do the same for twoPointBtn (add 2 points) and threePointBtn (add 3 points).
+When any scoring button is clicked:
+If possession is "home":
+Add the corresponding points to homeScore.
+Update homeScoreDisplay.
+Else if possession is "visitors":
+Add the corresponding points to visitorScore.
+Update visitorScoreDisplay.
+Toggle possession to the other team.
+Update possessionIndicator to reflect the change.
 Update scoreboard display function:
 
-A function to update the homeScoreDisplay and visitorScoreDisplay with the current values of homeScore and visitorScore.
+A function to update the homeScoreDisplay and visitorScoreDisplay.
+A function to update the possessionIndicator to visually highlight the team with possession.
+Initial setup:
+
+Call the update scoreboard display function to show the initial scores and possession.
 */
+
+
+
