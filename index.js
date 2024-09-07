b9cb84223d2929e594c2b927e599a9d6058fc2a6
Ed <silva.email@yahoo.com>
2024-09-07 04:58:11 -0500

    Corrected handleClick function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Get the score displays
-const homeScoreDisplay = document.querySelector('#home-score');
-const guestScoreDisplay = document.querySelector('#guest-score');
+const homeScoreDisplay = document.querySelector("#home-score");
+const guestScoreDisplay = document.querySelector("#guest-score");
 
 // Get the buttons
-const home1Button = document.querySelector('#home-1');
-const home2Button = document.querySelector('#home-2');
-const home3Button = document.querySelector('#home-3');
-const guest1Button = document.querySelector('#guest-1');
-const guest2Button = document.querySelector('#guest-2');
-const guest3Button = document.querySelector('#guest-3');
+const home1Button = document.querySelector("#home-1");
+const home2Button = document.querySelector("#home-2");
+const home3Button = document.querySelector("#home-3");
+const guest1Button = document.querySelector("#guest-1");
+const guest2Button = document.querySelector("#guest-2");
+const guest3Button = document.querySelector("#guest-3");
 
 let homeScore = 0;
 let guestScore = 0;
@@ -16,18 +16,34 @@ let guestScore = 0;
 // ... (Your logic to update scores and handle button clicks will go here)
 
 const handleClick = (e) => {
-    if (e.target ===home1Button){
-        console.log(home1Button)
-        console.log('I was clicked')
-    }else 
-        console.log('This code is crap')
-    
-}
+  if (e.target === home1Button) {
+    homeScore += 1;
+    homeScoreDisplay.textContent=homeScore
+    console.log(home1Button);
+    console.log(homeScore);
+    console.log("I was clicked");
+  } else if (e.target === home2Button) {
+        homeScore+=2
+      homeScoreDisplay.textContent = homeScore;
+    } else if (e.target === home3Button) {
+        homeScore+=3
+      homeScoreDisplay.textContent = homeScore;
+    }else if (e.target === guest1Button) {
+      guestScore+=1
+      guestScoreDisplay.textContent = guestScore;
+    }else if (e.target === guest2Button) {
+      guestScore+=2
+      guestScoreDisplay.textContent = guestScore;
+    }else if (e.target === guest3Button) {
+      guestScore+=3
+      guestScoreDisplay.textContent = guestScore;
+    }
+};
 
 // Attach event listeners to the buttons
-home1Button.addEventListener('click', handleClick);
-home2Button.addEventListener('click', handleClick);
-home3Button.addEventListener('click', handleClick);
-guest1Button.addEventListener('click', handleClick);
-guest2Button.addEventListener('click', handleClick);
-guest3Button.addEventListener('click', handleClick);
+home1Button.addEventListener("click", handleClick);
+home2Button.addEventListener("click", handleClick);
+home3Button.addEventListener("click", handleClick);
+guest1Button.addEventListener("click", handleClick);
+guest2Button.addEventListener("click", handleClick);
+guest3Button.addEventListener("click", handleClick);
